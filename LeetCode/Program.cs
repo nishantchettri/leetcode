85a33b50231dcb8d272808840525a93f902a6b58
Nishant Chettri <cNishant@aureustechsystems.com>
2020-02-11 22:07:20 -0700

    First commit

---
+++ LeetCode/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+	static int[] s = new int[] { 4, 3, 2, 7, 8, 2, 3, 1 };
+
+	static void Main(string[] args)
+	{
+		var res = FindDisappearedNumbers(s);
+		Console.WriteLine(string.Join(",",res));
+		Console.ReadLine();
+	}
+	static IList<int> FindDisappearedNumbers(int[] nums)
+	{
+		HashSet<int> sets = new HashSet<int>();
+		List<int> missingNums = new List<int>();
+		foreach (int num in nums)
+		{
+			sets.Add(num);
+		}
+
+		for(int i=1; i <= nums.Length; i++){
+			if (!sets.Contains(i))
+			{
+				missingNums.Add(i);
+			}
+		}
+		return missingNums;
+	}
+}
